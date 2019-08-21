929da60930c5e012bf378de793ecfb3c36d3ca70
ohadsc <ohad.schachtel@gmail.com>
2019-08-21 14:07:59 +0300

    feat(base64):images -> inline base64

--- vue.config.js
+++ vue.config.js
@@ -8,11 +8,26 @@ module.exports = {
       new webpack.optimize.LimitChunkCountPlugin({
       	maxChunks: 1
       })
-    ]
+	]
   },
   chainWebpack:
   	config => {
-  		config.optimization.delete('splitChunks')
+		const fontsRule = config.module.rule('images')
+		// clear all existing loaders.
+		// if you don't do this, the loader below will be appended to
+		// existing loaders of the rule.
+		fontsRule.uses.clear()
+	  
+		config.module
+		  .rule('images')
+		  .test(/\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/)
+		  .use('base64-inline-loader?limit=1000&name=[name].[ext]')
+		  .loader('base64-inline-loader')
+		  .tap(options => {
+			// modify the options...    
+			return options
+		   })
+		  .end()
   	},
   	filenameHashing: false,
   	css: {
